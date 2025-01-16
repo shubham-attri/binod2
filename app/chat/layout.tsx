42abd188e6c329b2e202a71cd41e61bb2cf45fe2
Shubham Attri <attri.shubham148@gmail.com>
2025-01-16 06:45:25 +0530

    removed sidebar

--- app/chat/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { SidebarLayout } from "@/components/layouts/sidebar-layout";
-
-export default function ChatLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <SidebarLayout>{children}</SidebarLayout>;
-} 
--- app/history/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { SidebarLayout } from "@/components/layouts/sidebar-layout";
-
-export default function HistoryLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <SidebarLayout>{children}</SidebarLayout>;
-} 
